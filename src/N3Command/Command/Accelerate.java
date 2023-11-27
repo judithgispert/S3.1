40f8eb5360f27c77ceacaf11158b951e5c68b07b
judithgispert <jugispert@gmail.com>
2023-11-27 19:45:17 +0100

    Accelerate implements command

---
+++ src/N3Command/Command/Accelerate.java
@@ -0,0 +1,20 @@
+package N3Command.Command;
+
+import N3Command.Parking.VehicleAbstract;
+
+public class Accelerate implements InterfaceCommand{
+
+	private VehicleAbstract vehicle;
+	private int speedAccelerate;
+	
+	public Accelerate (VehicleAbstract vehicle, int speedAccelerate) {
+		this.vehicle = vehicle;
+		this.speedAccelerate = speedAccelerate;
+	}
+	
+	@Override
+	public void execute() {
+		vehicle.accelerate(speedAccelerate);
+	}
+
+}
